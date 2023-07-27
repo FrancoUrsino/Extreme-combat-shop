c625a5203a83a9ded584ec8fa7a0b5e6fc3067c2
Franco Ursino <franncoursinno@gmail.com>
2023-07-27 11:41:50 -0300

    1er Pre entrega terminada

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ItemListContainer = ({greeting})=>{
   console.log(greeting)
   return(
-    <h1>{greeting}</h1>
+    <h1 className='text-6xl flex justify-center my-10'>{greeting}</h1>
   )
 }
 
